e93f6a085125013d97fc0f99223aaf53340026ec
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-08 08:58:13 +0900

    4-2

--- 4.rb
+++ 4.rb
@@ -1,4 +1,14 @@
-calculate_points(amount, is_birthday)
+def calculate_points(amount, is_birthday)
+  if amount < 1000
+    point = amount * 0.03
+  else
+    point = amount * 0.05
+  end
+  
+
+
+
+
 
 calculate_points(500, false)
 calculate_points(2000, false)
