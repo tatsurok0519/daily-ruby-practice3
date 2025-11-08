061ad16f707a5d5ac02107a954b9217f89ab10f1
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-09 05:49:03 +0900

    6-3

--- 6.rb
+++ 6.rb
@@ -3,6 +3,11 @@ def end_other(a, b)
   b_down = b.downcase
   a_len = a_down.length
   b_len = b_down.length
+  if a_down == b_down[-a_len..-1] || b_down == a_down[-b_len..-1]
+    puts true
+  else
+    puts false
+  end
   
 end
 
