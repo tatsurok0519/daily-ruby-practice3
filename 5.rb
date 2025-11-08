51fdab8907de9c1232af34452477d587ed499809
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-08 09:17:15 +0900

    5-2

--- 5.rb
+++ 5.rb
@@ -1,5 +1,17 @@
 def fizz_buzz
-  # ここに処理を書き加えてください
+  num = 0
+  while num <= 100 do
+    if num % 15 == 0
+      puts "FizzBuzz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    else
+      puts num
+    end
+  num += 1
+  end
 end
 
 fizz_buzz
