6f450ea9ff31d9bf628933859e1d1a1c7824f47f
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-09 06:19:27 +0900

    10-3

--- 10.rb
+++ 10.rb
@@ -21,7 +21,7 @@ puts "検索したい数字を入力してください"
 target = gets.to_i
 number_of_elements = array.length
 result = binary_search(array, number_of_elements, target)
-if result != -1
+if result == -1
   puts "#{target}は配列内に存在しません"
 else
   puts "#{target}は配列の#{result + 1}番目に存在します"
