ae521a7bbc3bf5a6a830cb4d4cb175459bad8cf6
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-10 15:24:51 +0900

    13

---
+++ 13.rb
@@ -0,0 +1,12 @@
+def get_days(year, month)
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  # ここに処理を書き加えてください
+end
+
+puts "年を入力してください："
+year = gets.to_i
+puts "月を入力してください："
+month = gets.to_i
+
+days = get_days(year, month)
+puts "#{year}年#{month}月は#{days}日間あります"
