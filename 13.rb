c155c3d7a152e51f74508946b812c521d21ae4cd
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-10 15:26:12 +0900

    13-2

--- 13.rb
+++ 13.rb
@@ -1,6 +1,18 @@
 def get_days(year, month)
   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-  # ここに処理を書き加えてください
+  if month == 2
+    if year % 400 == 0
+      return 29
+    elsif year % 100 == 0
+      return 28
+    elsif year % 4 == 0
+      return 29
+    else
+      return 28
+    end
+  else
+    return month_days[month - 1]
+  end
 end
 
 puts "年を入力してください："
