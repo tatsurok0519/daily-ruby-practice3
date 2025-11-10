bde9a800a8b873213536d15f3aebb5452967bf6f
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-10 15:29:57 +0900

    14-2

--- 14.rb
+++ 14.rb
@@ -1,5 +1,10 @@
-def close_far(a, b, c)
+def close_far(a, b, c) 
+  if ((a-b).abs == 1 || (a-c).abs == 1) && ((b-c).abs >= 2)
+    return true
+  else
+    return false
+  end
 
-close_far(1, 2, 10) → True
-close_far(1, 2, 3) → False
-close_far(4, 1, 3) → True
+close_far(1, 2, 10)
+close_far(1, 2, 3)
+close_far(4, 1, 3)
