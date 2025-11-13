d79caf0e264754f0ed4c436fd1cd249ee0e34359
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-14 08:53:26 +0900

    24-2

--- 24.rb
+++ 24.rb
@@ -1,6 +1,12 @@
 require "date"
 
 day = Date.today.wday
-days
+days = ["日","月","火","水","木","金","土"]
+
+  if day == 5
+    day = "今日は#{days[day]}曜日だ！！！"
+  else
+    day = "今日は#{days[day]}曜日"
+  end
 
 puts day
