3add8095cae6ec0fc5b0411850d1df690210031e
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-14 08:49:13 +0900

    23-2

--- 23.rb
+++ 23.rb
@@ -6,4 +6,14 @@ class Article
     @content = content
   end
 
+  def display
+    puts "著者: #{@author}"
+    puts "タイトル: #{@title}"
+    puts "本文: #{@content}"
+  end
+
 end
+
+著者: 阿部
+タイトル: Rubyの素晴らしさについて
+本文: Awesome Ruby!
