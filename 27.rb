e369191f5a553132a93f3c9591c45e89bd6cabed
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-14 08:59:43 +0900

    27

---
+++ 27.rb
@@ -0,0 +1,10 @@
+def search(target_num, input)
+  # 処理を記述
+end
+
+input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
+# 呼び出し例
+search(11, input)
+search(5, input)
+search(12, input)
+search(7, input)
