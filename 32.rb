111b5928b2ec6b26517d2a1fc01f9be8c6174e30
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-15 17:01:54 +0900

    32-4

--- 32.rb
+++ 32.rb
@@ -3,7 +3,7 @@ class Fruit
    puts "採れたて新鮮な果実です"
  end
 
- def initialize
+ def initialize(name, price)
    @name = name
    @price = price
  end
@@ -14,7 +14,11 @@ class Fruit
 end
 
 
-# 3つのインスタンスを生成してください
+apple = Fruit.new("リンゴ", 120)
+orange = Fruit.new("オレンジ", 200)
+strawberry = Fruit.new("イチゴ", 60) 
 
-# クラスメソッドを呼び出し、「採れたて新鮮な果実です」と表示してください
-# インスタンス毎にインスタンスメソッドを呼び出し、「【名前】は【価格】円です」と表示してください
+Fruit.flesh
+apple.introduce
+orange.introduce
+strawberry.introduce
